9686ec13788c187ee7ba4fa644d65429b2060b1a
Duncan Watson-Parris <duncan.watson-parris@physics.ox.ac.uk>
2016-12-15 10:47:05 +0000

    First draft of subsetting form with a region select widget and looking decent (using bootstrap).

--- subset/forms.py
+++ subset/forms.py
@@ -1,8 +1,15 @@
-from django import forms
+from django.contrib.gis import forms
 import datasets.models as model
 
 
 class DataSelection(forms.Form):
-    dummy = forms.CharField(label='Your name', max_length=100)
-    # dataset = forms.Select(choices=model.Dataset)
+    # Select the dataset
+    dataset = forms.ModelChoiceField(queryset=model.Dataset.objects.all())
 
+    # All of these need to be updated based on the above...
+    variables = forms.ModelMultipleChoiceField(queryset=model.MeasurementVariable.objects.all())
+    start_date = forms.DateField(widget=forms.SelectDateWidget())
+    end_date = forms.DateField(widget=forms.SelectDateWidget())
+
+    # I'd like to restrict this to a rectangle somehow... and probably have the option for text entry
+    region = forms.PolygonField(widget=forms.OSMWidget(attrs={'map_width': 800, 'map_height': 500}))
