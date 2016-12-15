ce5ba4d60a9690d233e9c02b063dfde2e9d1d2a6
Duncan Watson-Parris <duncan.watson-parris@physics.ox.ac.uk>
2016-12-15 10:43:39 +0000

    Register the dataset model objects to the admin page for easy management

--- datasets/admin.py
+++ datasets/admin.py
@@ -1,3 +1,14 @@
-from django.contrib import admin
-
 # Register your models here.
+from django.contrib.gis import admin
+from .models import *
+
+# Use the default admin page with a Vector Map Level 0 WMS dataset background
+# default_admin = admin.GeoModelAdmin
+
+default_admin = admin.OSMGeoAdmin
+
+# Use the Open-Street map admin page
+admin.site.register(Dataset, default_admin)
+admin.site.register(MeasurementDataset, admin.ModelAdmin)
+admin.site.register(MeasurementVariable, admin.ModelAdmin)
+admin.site.register(MeasurementFile, default_admin)
