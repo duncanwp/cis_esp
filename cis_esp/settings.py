70cdcc037758f033233ad16e4b43242e241893ab
Duncan Watson-Parris <duncan.watson-parris@physics.ox.ac.uk>
2017-01-03 12:21:02 +0000

    We need to specify the static root when collecting static files

--- cis_esp/settings.py
+++ cis_esp/settings.py
@@ -148,4 +148,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 GDAL_LIBRARY_PATH = r'C:\Users\duncan\Anaconda3\envs\cis_esp\Library\bin\gdal201.dll'
