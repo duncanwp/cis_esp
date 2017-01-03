bcd7a77d354d6556601c5999f7d78e8b73bb10ca
Duncan Watson-Parris <duncan.watson-parris@physics.ox.ac.uk>
2017-01-03 15:55:21 +0000

    Adding nice serializer for platform type

--- datasets/serializers.py
+++ datasets/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
-from .models import Dataset
+from .models import Dataset, Measurement
 
 
 class DatasetSerializer(serializers.ModelSerializer):
+
+    platform_type = serializers.SerializerMethodField()
+
     class Meta:
         model = Dataset
         fields = ('id', 'name', 'platform_type', 'platform_name', 'region', 'public')
+
+    def get_platform_type(self, obj):
+        return obj.get_platform_type_display()
