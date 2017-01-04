66012f85748db9043cfaa10af9c4cee9f43b20dc
Duncan Watson-Parris <duncan@watson-parris.co.uk>
2017-01-04 17:00:33 +0000

    Make the CALIOP region a collection to fit the data model

--- web/datasets/load_CALIOP.py
+++ web/datasets/load_CALIOP.py
@@ -7,6 +7,7 @@ def load_caliop_data(dirpath, test_set=False):
     from .utils import GLOBAL_EXTENT
     from os.path import join
     from django.contrib.auth.models import User
+    from django.contrib.gis.geos import GeometryCollection
     files = glob.glob(join(dirpath, "*.hdf.met"))
 
     if test_set:
@@ -17,8 +18,8 @@ def load_caliop_data(dirpath, test_set=False):
     d = Dataset.objects.create(time_start=datetime(2008,1,1,0,0,0), time_end=datetime(2008,12,31,0,0,0),
                                platform_type='SA', source='NASA', public=True, name='CALIOP L2 Aerosol Profile V4',
                                project_URL='https://www-calipso.larc.nasa.gov/resources/calips', region='Global',
-                               spatial_extent=GLOBAL_EXTENT.wkt, owner=User.objects.filter(username='duncan').first(),
-                               campaign=c)
+                               spatial_extent=GeometryCollection(GLOBAL_EXTENT).wkt,
+                               owner=User.objects.filter(username='duncan').first(), campaign=c)
 
     pbc = Measurement(measurement_type='PBC', dataset=d)
     tbc = Measurement(measurement_type='TBC', dataset=d)
