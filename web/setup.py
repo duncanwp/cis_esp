4641d4e91de4e80f3a7b5599874ea2abbb2a56af
Duncan Watson-Parris <duncan.watson-parris@physics.ox.ac.uk>
2018-03-07 09:50:34 +0000

    Bumping minor version

--- web/setup.py
+++ web/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='cis_esp',
-    version=0.1,
+    version=0.2,
     description='CIS Evaluation and data Synthesis Platform',
     author='Duncan Watson-Parris',
     classifiers=[
